95848db3442a1d46f9bfc9a19f60e06a20721441
IkahnKakap <krf2308@gmail.com>
2023-12-25 19:04:28 +0700

    signin

--- app/src/main/java/com/example/myapplication/SigninActivity.java
+++ app/src/main/java/com/example/myapplication/SigninActivity.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -11,6 +12,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -20,6 +27,7 @@ public class SigninActivity extends AppCompatActivity {
     private EditText inputUsername, inputPassword;
     Button btnsignin, btnsignup;
 
+    private DatabaseReference mDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,7 +43,40 @@ public class SigninActivity extends AppCompatActivity {
         btnsignin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                checkValidasi();
+                String username = inputUsername.getText().toString();
+                String password = inputPassword.getText().toString();
+
+                mDatabase = FirebaseDatabase.getInstance().getReference("User");
+
+                if (username.isEmpty()){
+                    showError(inputUsername, "Your Username can't be empty.");
+                }
+                if(password.isEmpty()){
+                    showError(inputPassword, "Password can't be empty.");
+                }
+
+                mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if(snapshot.child(username).exists()){
+                            if(snapshot.child(username).child("Password").getValue(String.class).equals(password)){
+                                Toast.makeText(getApplicationContext(), "login successfull", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(SigninActivity.this, HomepageActivity.class);
+                                startActivity(intent);
+                            } else{
+                                Toast.makeText(getApplicationContext(), "username or password is incorrect", Toast.LENGTH_SHORT).show();
+                            }
+                        } else{
+                            Toast.makeText(getApplicationContext(), "username or password is incorrect", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
             }
         });
         btnsignup = findViewById(R.id.button_signup);
@@ -48,51 +89,8 @@ public class SigninActivity extends AppCompatActivity {
 
         });
     }
-    private void checkValidasi(){
-        String username = inputUsername.getText().toString();
-        String password = inputPassword.getText().toString();
-
-        if (username.isEmpty()){
-            showError(inputUsername, "Your Username can't be empty.");
-        }
-        else if(username.length() < 8) {
-            showError(inputUsername, "Your Username must be at least 8 characters.");
-        }
-        if(password.isEmpty()){
-            showError(inputPassword, "Password can't be empty.");
-        }
-        else if(password.length() < 8){
-            showError(inputPassword, "Password must be at least 8 characters");
-        }
-        else if(!containsSymbol(password)){
-            showError(inputPassword, "Password must contains at least 1 symbol");
-        }
-        else if(!containsCapitalLetter(password)){
-            showError(inputPassword, "Password must contains at least 1 capital letter");
-        }
-        else{
-            Toast.makeText(this, "Registration Success", Toast.LENGTH_SHORT).show();
-            Intent intent = new Intent(SigninActivity.this, HomepageActivity.class);
-            startActivity(intent);
-        }
-    }
     private void showError(EditText input, String s){
         input.setError(s);
         input.requestFocus();
-    }
-    private boolean containsSymbol(String password) {
-        Pattern pattern = Pattern.compile("[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]");
-        Matcher matcher = pattern.matcher(password);
-        return matcher.find();
-    }
-    private boolean containsCapitalLetter(String password) {
-        Pattern pattern = Pattern.compile("[A-Z]");
-        Matcher matcher = pattern.matcher(password);
-        return matcher.find();
-    }
-    private boolean containsNumber(String password) {
-        Pattern pattern = Pattern.compile("[0-9]");
-        Matcher matcher = pattern.matcher(password);
-        return matcher.find();
     }
 }
